8b2ae75d8a571d9e8b2698368bf0d9a00547f3eb
Mathias Henriksen <mat-d-h@hotmail.com>
2020-11-20 09:40:13 +0100

    Removed unused method, and applied auto-formatting

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,23 +1,21 @@
 module.exports = (sequelize, Sequelize) => {
     const Register = sequelize.define("users", {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      username:{
-        type: Sequelize.STRING,
-        required: true
-      },
-      password:{
-        type: Sequelize.STRING,
-        required: true
-      }
-    },
-    {
-      timestamps: false
-   }
-   );
-  
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: Sequelize.STRING,
+            required: true
+        },
+        password: {
+            type: Sequelize.STRING,
+            required: true
+        }
+    }, {
+        timestamps: false
+    });
+
     return Register;
-  };
+};
